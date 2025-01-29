da9bbe1de3975b77a85be0913c66f391608668be
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2025-01-29 22:52:38 +0200

    added comments models

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,14 @@
 from datetime import datetime
-from sqlalchemy import Boolean, DateTime, create_engine, Column, Integer, String, ForeignKey, Sequence
+from sqlalchemy import (
+    Boolean,
+    DateTime,
+    create_engine,
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    Sequence,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
@@ -11,34 +20,30 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-badge_id_seq = Sequence('badge_id_seq')
-challenge_id_seq = Sequence('challenge_id_seq')
-resource_id_seq = Sequence('resource_id_seq')
-users_id_seq = Sequence('users_id_seq')
-tag_id_seq = Sequence('tag_id_seq')
+badge_id_seq = Sequence("badge_id_seq")
+challenge_id_seq = Sequence("challenge_id_seq")
+resource_id_seq = Sequence("resource_id_seq")
+users_id_seq = Sequence("users_id_seq")
+tag_id_seq = Sequence("tag_id_seq")
 
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, Sequence('users_id_seq'),
-                primary_key=True, index=True)
+    id = Column(Integer, Sequence("users_id_seq"), primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
     role = Column(String)
     score = Column(Integer, default=0)
-    badges = relationship("Badge", secondary="userbadge",
-                          back_populates="users")
+    badges = relationship("Badge", secondary="userbadge", back_populates="users")
 
 
 class Badge(Base):
     __tablename__ = "badges"
-    id = Column(Integer, Sequence('badge_id_seq'),
-                primary_key=True, index=True)
+    id = Column(Integer, Sequence("badge_id_seq"), primary_key=True, index=True)
     title = Column(String, unique=True, index=True)
     description = Column(String)
-    users = relationship("User", secondary="userbadge",
-                         back_populates="badges")
+    users = relationship("User", secondary="userbadge", back_populates="badges")
 
 
 class Challenge(Base):
@@ -50,61 +55,55 @@ class Challenge(Base):
     output = Column(String)
     difficulty = Column(String)
     language = Column(String)
-    tags = relationship("Tag", secondary="challengetag",
-                        back_populates="challenges")
+    tags = relationship("Tag", secondary="challengetag", back_populates="challenges")
 
 
 class Tag(Base):
     __tablename__ = "tags"
-    id = Column(Integer, Sequence('tag_id_seq'), primary_key=True, index=True)
+    id = Column(Integer, Sequence("tag_id_seq"), primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     challenges = relationship(
-        "Challenge", secondary="challengetag", back_populates="tags")
-    resources = relationship(
-        "Resource", secondary="resourcetag", back_populates="tags")
+        "Challenge", secondary="challengetag", back_populates="tags"
+    )
+    resources = relationship("Resource", secondary="resourcetag", back_populates="tags")
 
 
 class ChallengeTag(Base):
     __tablename__ = "challengetag"
-    challenge_id = Column(Integer, ForeignKey(
-        'challenges.id'), primary_key=True)
-    tag_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
+    challenge_id = Column(Integer, ForeignKey("challenges.id"), primary_key=True)
+    tag_id = Column(Integer, ForeignKey("tags.id"), primary_key=True)
 
 
 class Resource(Base):
     __tablename__ = "resources"
-    id = Column(Integer, Sequence('resource_id_seq'),
-                primary_key=True, index=True)
+    id = Column(Integer, Sequence("resource_id_seq"), primary_key=True, index=True)
     title = Column(String, unique=True, index=True)
     description = Column(String)
-    tags = relationship("Tag", secondary="resourcetag",
-                        back_populates="resources")
+    tags = relationship("Tag", secondary="resourcetag", back_populates="resources")
 
 
 class ResourceTag(Base):
     __tablename__ = "resourcetag"
-    resource_id = Column(Integer, ForeignKey(
-        'resources.id'), primary_key=True)
-    tag_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
+    resource_id = Column(Integer, ForeignKey("resources.id"), primary_key=True)
+    tag_id = Column(Integer, ForeignKey("tags.id"), primary_key=True)
 
 
 class Friend(Base):
     __tablename__ = "friends"
-    user_id1 = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    user_id2 = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    user_id1 = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    user_id2 = Column(Integer, ForeignKey("users.id"), primary_key=True)
 
 
 class UserBadge(Base):
     __tablename__ = "userbadge"
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    badge_id = Column(Integer, ForeignKey('badges.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    badge_id = Column(Integer, ForeignKey("badges.id"), primary_key=True)
 
 
 class UserChallenge(Base):
     __tablename__ = "userchallenge"
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    challenge_id = Column(Integer, ForeignKey(
-        'challenges.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    challenge_id = Column(Integer, ForeignKey("challenges.id"), primary_key=True)
 
 
 Base.metadata.create_all(bind=engine)
@@ -125,3 +124,28 @@ class Notification(Base):
 
 
 User.notifications = relationship("Notification", back_populates="recipient")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, Sequence("comments_id_seq"), primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    comment = Column(String(255))
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="comments")
+
+
+class ChallengeComment(Base):
+    __tablename__ = "challangecomment"
+
+    challenge_id = Column(Integer, ForeignKey("challenges.id"), primary_key=True)
+    comment_id = Column(Integer, ForeignKey("comments.id"), primary_key=True)
+
+
+class ResourceComment(Base):
+    __tablename__ = "resourcecomment"
+
+    resource_id = Column(Integer, ForeignKey("resources.id"), primary_key=True)
+    comment_id = Column(Integer, ForeignKey("comments.id"), primary_key=True)
